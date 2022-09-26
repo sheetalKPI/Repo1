da54c1be26f47e2fc95d28bccb8849293d168f75
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-09-26 05:46:57 -0700

    commit

---
+++ ObjectOrientend/src/Oops/Rectangle.java
@@ -0,0 +1,12 @@
+package Oops;
+
+public class Rectangle {
+	public static void main(String ards[])
+	{
+		int width=5;
+		int height=10;
+		int area=width*height;
+		System.out.println("Area of rectangle="+area);
+	}
+
+}
---
+++ ObjectOrientend/src/Oops/Square.java
@@ -0,0 +1,11 @@
+package Oops;
+
+public class Square {
+	public static void main(String args[])
+	{
+		int s=12;
+		int area_square=s*s;
+		System.out.println("Area of Square="+area_square);
+	}
+
+}
