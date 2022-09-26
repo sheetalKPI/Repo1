e1a271975428c7e27faad3877bd1c039d5e1f282
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-09-26 03:53:20 -0700

    commit

---
+++ ObjectOrientend/src/Oops/TruckSC.java
@@ -0,0 +1,12 @@
+package Oops;
+
+public class TruckSC extends Truck{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Truck tata=new TruckSC();
+tata.speed();
+tata.mileage();
+	}
+
+}
