1aed2b00fb1f815b666615871acf351e4e455117
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-09-27 05:34:16 -0700

    Commit for "Constructor & interface"

--- Oops2/src/practice/Constructor.java
+++ Oops2/src/practice/Constructor.java
@@ -6,4 +6,13 @@ public class Constructor {
 		// TODO Auto-generated constructor stub
 	}
 
-}
+	public Constructor(int a, int b) {
+		
+	}
+	public static int Constructor() {
+		return 1;
+	}
+	public static void main(String[] args) {
+		Constructor test= new Constructor();
+	}
+	}
